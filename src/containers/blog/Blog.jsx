d96a10cd89bf7eed735644adc014abd0a9c9d578
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-28 21:10:59 +0200

    fixed a bug which was making it not rendering

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,20 +1,21 @@
-// import React from 'react'
-// import './blog.css';
-// import Article from '../../../../project_modern_ui_ux_gpt3/src/components/article/Article';
+import React from 'react'
+import './blog.css';
+import Article from '../../components/article/Article';
+import blog1 from '../../assets/blog01.png';
 
-// const Blog = () => {
-//   return (
-//    <div className='gpt3__blog section__padding' id='blog'>
-//     <div className='gpt3__blog-heading'>
-//       <h1 className='gradient__text'>A lot is happening,<br /> We are blogging about it.</h1>
-//     </div>
-//     <div className='gpt3__blog-container'>
-//       <div className='gpt3__blog-container_groupA'>
-//         <Article imgUrl={blog1} date="Sep 26,2021" text="GPT-3 and Open AI is the future.Let us Explore how it is?" />
-//       </div>
-//     </div>
-//    </div>
-//   )
-// }
+const Blog = () => {
+  return (
+   <div className='gpt3__blog section__padding' id='blog'>
+    <div className='gpt3__blog-heading'>
+      <h1 className='gradient__text'>A lot is happening,<br /> We are blogging about it.</h1>
+    </div>
+    <div className='gpt3__blog-container'>
+      <div className='gpt3__blog-container_groupA'>
+        <Article imgUrl={blog1} date="Sep 26,2021" text="GPT-3 and Open AI is the future.Let us Explore how it is?" />
+      </div>
+    </div>
+   </div>
+  )
+}
 
-// export default Blog
+export default Blog
