fe2caaa99466b768ccdf130e3f34866514e0aa67
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-28 16:42:21 +0200

    added blog element and brand element

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import './blog.css';
+// import React from 'react'
+// import './blog.css';
+// import Article from '../../../../project_modern_ui_ux_gpt3/src/components/article/Article';
 
-const Blog = () => {
-  return (
-    <section id='blog' className='section__padding'>
-      Blog
-    </section>
-  )
-}
+// const Blog = () => {
+//   return (
+//    <div className='gpt3__blog section__padding' id='blog'>
+//     <div className='gpt3__blog-heading'>
+//       <h1 className='gradient__text'>A lot is happening,<br /> We are blogging about it.</h1>
+//     </div>
+//     <div className='gpt3__blog-container'>
+//       <div className='gpt3__blog-container_groupA'>
+//         <Article imgUrl={blog1} date="Sep 26,2021" text="GPT-3 and Open AI is the future.Let us Explore how it is?" />
+//       </div>
+//     </div>
+//    </div>
+//   )
+// }
 
-export default Blog
+// export default Blog
