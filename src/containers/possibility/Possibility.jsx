3845137caa3f3f1a334787447c55dc736aba40b5
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-29 20:46:00 +0200

    added possibility page

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import PossibilityImage from '../../assets/possibility.png';
 import './possibility.css';
-const Possibility = () => {
-  return (
-    <section id='possibility' className='section__padding'>
-      Possibility
-    </section>
-  )
-}
 
-export default Possibility
+const Possibility = () =>(
+  <div className='gpt3__possibility section__padding' id='possibility'>
+    <div className='gpt3__possibility-image'>
+      <img src={PossibilityImage} alt="possibility" />
+    </div>
+    <div className='gpt3__possibility-content'>
+      <h4>Request Early Access to Get started</h4>
+      <h1 className='gradient__text'>The possibilities are <br /> beyond your imagination</h1>
+      <p>Yet bed any for traveling assistance indulgence unpleasing. Not thoughts all exercise blessing.Indulgence way everything joy alteration boisterous the attachment.Party we years  to order allow asked of . </p>
+      <h4>Request Early Access to Get Started</h4>
+    </div>
+  </div>
+);
+export default Possibility;
