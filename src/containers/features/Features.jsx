57c19294b2110fede699881765bb760d459f6bc1
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-29 20:03:59 +0200

    fixed a bug

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -25,7 +25,7 @@ const featuresData =[
 const Features = () =>(
   <div  className='gpt3__features section __padding' id='features'>
     <div className='gpt3__features-heading'>
-      <h1 className='gradient_text'>
+      <h1 className='gradient__text'>
         The Future is Now and You Just Need to Realize It. Step into Future Today .& Make it Happen.
       </h1>
       <p>Request Early Access to Get Started</p>
