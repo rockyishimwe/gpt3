93e588ca53fca2f80dbe9d01b3a1a6f40133330b
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-30 19:44:53 +0200

    implemented the blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './blog.css';
-import Article from '../../components/article/Article';
-import {blog1,blog02,blog03,blog04,blog05} from './imports';
+import {Article} from '../../components';
+import {blog01,blog02,blog03,blog04,blog05} from './imports';
 
 
 const Blog = () => {
@@ -12,7 +12,7 @@ const Blog = () => {
     </div>
     <div className='gpt3__blog-container'>
       <div className='gpt3__blog-container_groupA'>
-        <Article imgUrl={blog1} date="Sep 26,2021" text="GPT-3 and Open AI is the future.Let us Explore how it is?" />
+        <Article imgUrl={blog01} date="Sep 26,2021" text="GPT-3 and Open AI is the future.Let us Explore how it is?" />
       </div>
       <div className='gpt3__blog-container_groupB'>
         
