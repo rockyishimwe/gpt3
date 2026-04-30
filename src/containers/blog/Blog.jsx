b034f6874d6cf4c01f9246a1065a458e78cbf9a5
rocky ishimwe <rockyishimwe9@gmail.com>
2026-04-30 09:00:57 +0200

    added a blog section and styled it

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import './blog.css';
 import Article from '../../components/article/Article';
-import blog1 from '../../assets/blog01.png';
+import {blog1,blog02,blog03,blog04,blog05} from './imports';
+
 
 const Blog = () => {
   return (
    <div className='gpt3__blog section__padding' id='blog'>
     <div className='gpt3__blog-heading'>
-      <h1 className='gradient__text'>A lot is happening,<br /> We are blogging about it.</h1>
+      <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
     </div>
     <div className='gpt3__blog-container'>
       <div className='gpt3__blog-container_groupA'>
         <Article imgUrl={blog1} date="Sep 26,2021" text="GPT-3 and Open AI is the future.Let us Explore how it is?" />
+      </div>
+      <div className='gpt3__blog-container_groupB'>
+        
+        <Article imgUrl={blog02} date='Sep 26,2021' text="GPT-3 and Open AI is the future. Let us Explore how it is?"/>
+        <Article imgUrl={blog03} date='Sep 26,2021' text="GPT-3 and Open AI is the future. Let us Explore how it is?"/>
+        <Article imgUrl={blog04} date='Sep 26,2021' text="GPT-3 and Open AI is the future. Let us Explore how it is?"/>
+        <Article imgUrl={blog05} date='Sep 26,2021' text="GPT-3 and Open AI is the future. Let us Explore how it is?"/>
+
       </div>
     </div>
    </div>
