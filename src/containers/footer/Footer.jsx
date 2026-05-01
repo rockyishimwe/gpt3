f635389695572e7662e59455d8bafad585f67481
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-01 22:05:10 +0200

    added a footer'

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React from 'react';
+import gpt3Logo from '../../assets/logo.svg';
 import './footer.css';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
-
-export default Footer
+const Footer = () =>(
+  <div className='gpt3__footer section__padding'>
+    <div className='gpt3__footer-heading'>
+      <h1 className='gradient__text'>
+        Do you want to step in to the future before others
+      </h1>
+    </div>
+    <div className='gpt3__footer-btn'>
+      <p>Request Early Access</p>
+    </div>
+    <div className='gpt3__footer-links'>
+      <div className='gpt3__footer-links_log'>
+        <img src={gpt3Logo} alt="gpt3_log" />
+        <p>Kigali . Rwanda ,<br />All Rights Reserved</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <h4>Links</h4>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className='gpt3__footer-links_div'>
+        <h4>company</h4>
+        <p>Terms & Conditions</p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className='gpt3__footer-links_div'>
+        <h4>Get in touch</h4>
+        <p> kigali . Rwanda</p>
+        <p>+250 791 039 241</p>
+        <p>rockyishimwe9@gmail.com</p>
+      </div>
+    </div>
+    <div className='gpt3__footer-copyright'>
+      <p>@2026 GPT-3.All Rights reserved.</p>
+    </div>
+  </div>
+);
+export default Footer;
